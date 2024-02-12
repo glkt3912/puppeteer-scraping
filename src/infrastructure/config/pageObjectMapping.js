95785fec92ac3913e9d1a563d6caf522a03121ad
glkt <mole.gunma@gmail.com>
2024-02-12 19:04:45 +0900

    Use Config

---
+++ src/infrastructure/config/pageObjectMapping.js
@@ -0,0 +1,14 @@
+const pageObjectMapping = {
+  cpu: '../infrastructure/scraping/pageObjects/CpuPage.js',
+  gpu: '../infrastructure/scraping/pageObjects/GpuPage.js',
+  motherboard: '../infrastructure/scraping/pageObjects/MotherboardPage.js',
+  power: '../infrastructure/scraping/pageObjects/PowerPage.js',
+  memory: '../infrastructure/scraping/pageObjects/MemoryPage.js',
+  hdd: '../infrastructure/scraping/pageObjects/HddPage.js',
+  ssd: '../infrastructure/scraping/pageObjects/SsdPage.js',
+  cooler: '../infrastructure/scraping/pageObjects/CoolerPage.js',
+  pccase: '../infrastructure/scraping/pageObjects/PcCasePage.js',
+  display: '../infrastructure/scraping/pageObjects/DisplayPage.js',
+};
+
+export default pageObjectMapping;
