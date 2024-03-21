d782a6bd038602b25e48f9bafc3cacb576125084
glkt <mole.gunma@gmail.com>
2024-03-22 01:03:36 +0900

    Restore scrapingUrls.js

---
+++ src/infrastructure/config/scrapingUrls.js
@@ -0,0 +1,14 @@
+const scrapingUrls = {
+    cpu: 'https://kakaku.com/pc/cpu/ranking_0510/',
+    gpu: 'https://kakaku.com/pc/videocard/ranking_0550/',
+    motherboard: 'https://kakaku.com/pc/motherboard/ranking_0540/',
+    power: 'https://kakaku.com/pc/power-supply/ranking_0590/',
+    memory: 'https://kakaku.com/pc/pc-memory/ranking_0520/',
+    hdd: 'https://kakaku.com/pc/hdd-35inch/ranking_0530/',
+    ssd: 'https://kakaku.com/pc/ssd/ranking_0537/',
+    cooler: 'https://kakaku.com/pc/cpu-cooler/ranking_0512/',
+    pccase: 'https://kakaku.com/pc/pc-case/ranking_0580/',
+    display: 'https://kakaku.com/pc/gaming-monitor/ranking_V085/',
+  };
+  
+  export default scrapingUrls;
